4f902f211c1250e24529f72a26b026ef886e9071
kissan-beej-bhandar <vipinrao03@gmail.com>
2022-11-15 21:00:16 +0530

    change update need

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const link = "playlink.fun"; 
 const appLink = "https://play.google.com/store/apps/details?id=com.vipinrao.incognito";
 const VersionCode = "vipin1.0.0";
-const updateNeed = 1; // 0 - no need , 1 - not compulsary to update , 2 - compulsary to update
+const updateNeed = 0; // 0 - no need , 1 - not compulsary to update , 2 - compulsary to update
 
 
 app.use(cors());
